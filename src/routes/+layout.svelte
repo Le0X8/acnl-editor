935d0b95c459d9f57eb983051b954688c3c6a016
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-11-30 18:01:58 +0100

    Added license and stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import '$styles/global.scss';
+
+  let { children } = $props();
+</script>
+
+<noscript>
+  Please enable JavaScript to view this page.
+</noscript>
+
+{@render children()}
